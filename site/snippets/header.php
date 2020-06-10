540dce448d2cc5fd0b67190ebacb3355a03f8eac
Jens Küsters <jens@kuesters.net>
2020-06-10 12:50:04 +0200

    favicons and fonts update

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,6 +9,11 @@ use Kirby\Cms\Page;
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="theme-color" content="#3b5883">
+  <link rel="icon" href="<?= url('/favicon.svg') ?>">
+  <link rel="mask-icon" href="<?= url('/mask-icon.svg') ?>" color="#3b5883">
+  <link rel="apple-touch-icon" href="<?= url('/apple-touch-icon.png') ?>">
+  <link rel="manifest" href="<?= url('/manifest.json') ?>">
   <?= $page->metaTags() ?>
 </head>
 <body class="bg-gray-200">
