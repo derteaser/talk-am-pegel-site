f2729e256bb85f0297895c7465118bd56d299179
Jens Küsters <jens@kuesters.net>
2021-05-05 10:11:30 +0200

    Update latest-event.php

--- site/snippets/latest-event.php
+++ site/snippets/latest-event.php
@@ -17,7 +17,7 @@ if ($text = $event->text() && $blocks = $event->text()->toBlocks()) {
   <a href="<?= $event->url() ?>" class="group">
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center" data-aos="fade-in">
-        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"><?= $event->title()->widont() ?>
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-100"><?= $event->title()->widont() ?>
         </h1>
         <p class="mb-8 leading-relaxed max-w-screen-sm"><?= $description ?></p>
         <button class="inline-flex text-white bg-tap-red-500 border-0 py-2 px-6 focus:outline-none group-hover:bg-tap-red-800 rounded text-lg">Mehr erfahren</button>
