989c1557911119523cf3e96072b3a3a62569e915
derteaser <derteaser@users.noreply.github.com>
2021-05-13 10:34:43 +0000

    Apply theme changes

---
+++ public/dist/assets/shared.06d15565.js
@@ -0,0 +1,1 @@
+import{A as r}from"./vendor.d7096b12.js";r.init({delay:200,duration:1e3,once:!0,mirror:!1});
--- public/dist/assets/shared.c25fa61b.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./vendor.b46c9357.js";r.init({delay:200,duration:1e3,once:!0,mirror:!1});
