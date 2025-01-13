56006ac09dc88f0dd12d8c3ce0d55192a105603d
Jens Küsters <jens@kuesters.net>
2025-01-13 12:02:56 +0100

    ♻️ Refactor event template for improved structure and styling consistency

--- site/templates/event.php
+++ site/templates/event.php
@@ -5,31 +5,31 @@
 <?php snippet('layout', slots: true); ?>
 
 <section class="text-gray-700">
-  <div class="container px-5 pt-6 pb-12 mx-auto flex flex-col">
-    <div class="lg:w-4/6 mx-auto">
+  <div class="container flex flex-col px-5 pt-6 pb-12 mx-auto">
+    <div class="mx-auto lg:w-4/6">
       <?php if ($page->date()->toDate() > time()): ?>
-        <div class="flex flex-col sm:flex-row mt-10">
-          <div class="sm:w-2/3 sm:pr-8 sm:py-8 sm:border-r border-gray-300 sm:border-b-0 border-b mb-4 pb-4 sm:mb-0">
+        <div class="flex flex-col mt-10 sm:flex-row">
+          <div class="pb-4 mb-4 border-b border-gray-300 sm:w-2/3 sm:pr-8 sm:py-8 sm:border-r sm:border-b-0 sm:mb-0">
             <div class="mb-4 blocks"><?= $page->text()->toBlocks() ?></div>
           </div>
-          <div class="sm:w-1/3 text-center sm:pl-8 sm:py-12">
+          <div class="text-center sm:w-1/3 sm:pl-8 sm:py-12">
             <div
-              class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 dark:bg-gray-700 text-gray-400">
+              class="inline-flex items-center justify-center w-20 h-20 text-gray-400 bg-gray-200 rounded-full dark:bg-gray-700">
               <?php snippet('icons/ticket-2-line', ['class' => 'fill-current size-16']); ?>
             </div>
-            <div class="flex flex-col items-center text-center justify-center">
-              <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">Anmeldung</h2>
-              <div class="w-12 h-1 bg-tap-blue-500 rounded mt-2 mb-4"></div>
+            <div class="flex flex-col items-center justify-center text-center">
+              <h2 class="mt-4 text-lg font-medium text-gray-900 title-font">Anmeldung</h2>
+              <div class="w-12 h-1 mt-2 mb-4 rounded bg-tap-blue-500"></div>
               <p class="text-base text-gray-600 dark:text-gray-300">Für die Teilnahme an unserer Veranstaltung ist eine
                 Anmeldung zwingend erforderlich. Bitte bestellen Sie nachfolgend Ihr kostenloses Ticket über unseren
                 Partner Eventbrite!</p>
-              <a class="btn btn-red mt-8" href="<?= $page->eventbrite_url() ?>" target="_blank"
+              <a class="mt-8 btn btn-red" href="<?= $page->eventbrite_url() ?>" target="_blank"
                  rel="noopener noreferrer" onclick="fathom.trackGoal('DZC4R54V', 0);">Kostenloses Ticket sichern</a>
             </div>
           </div>
         </div>
       <?php else: ?>
-        <div class="mt-10 mb-4 blocks mx-auto"><?= $page->text()->toBlocks() ?></div>
+        <div class="mx-auto mt-10 mb-4 blocks"><?= $page->text()->toBlocks() ?></div>
       <?php endif; ?>
     </div>
   </div>
@@ -38,44 +38,44 @@
 <section class="text-gray-700">
   <div class="container px-5 py-12 mx-auto">
     <div class="flex flex-wrap -m-4">
-      <div class="p-4 w-full md:w-1/2 lg:w-1/3" data-aos="zoom-in" data-aos-delay="100">
-        <div class="flex rounded-lg h-full bg-gray-100 dark:bg-gray-800 p-8 flex-col">
+      <div class="w-full p-4 md:w-1/2 lg:w-1/3" data-aos="zoom-in" data-aos-delay="100">
+        <div class="flex flex-col h-full p-8 bg-gray-100 rounded-lg dark:bg-gray-800">
           <div class="flex items-center">
             <div
-              class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-tap-blue-500 text-white flex-shrink-0">
+              class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 mr-3 text-white rounded-full bg-tap-blue-500">
               <?php snippet('icons/calendar-event-fill', ['class' => 'size-5']); ?>
             </div>
-            <p class="text-gray-900 dark:text-gray-200 text-lg font-medium"><?= $page
-                ->date()
-                ->toDate('%e. %B %Y') ?></p>
+            <p class="text-lg font-medium text-gray-900 dark:text-gray-200"><?= $page
+              ->date()
+              ->toDate('%e. %B %Y') ?></p>
           </div>
         </div>
       </div>
-      <div class="p-4 w-full md:w-1/2 lg:w-1/3" data-aos="zoom-in" data-aos-delay="300">
-        <div class="flex rounded-lg h-full bg-gray-100 dark:bg-gray-800 p-8 flex-col">
+      <div class="w-full p-4 md:w-1/2 lg:w-1/3" data-aos="zoom-in" data-aos-delay="300">
+        <div class="flex flex-col h-full p-8 bg-gray-100 rounded-lg dark:bg-gray-800">
           <div class="flex items-center">
             <div
-              class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-tap-blue-500 text-white flex-shrink-0">
+              class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 mr-3 text-white rounded-full bg-tap-blue-500">
               <?php snippet('icons/time-fill', ['class' => 'size-5']); ?>
             </div>
-            <p class="text-gray-900 dark:text-gray-200 text-lg font-medium"><?= $page
-                ->date()
-                ->toDate('%H:%M') ?> Uhr</p>
+            <p class="text-lg font-medium text-gray-900 dark:text-gray-200"><?= $page
+              ->date()
+              ->toDate('%H:%M') ?> Uhr</p>
           </div>
         </div>
       </div>
-      <div class="p-4 w-full lg:w-1/3" data-aos="zoom-in" data-aos-delay="500">
-        <div class="flex rounded-lg h-full bg-gray-100 dark:bg-gray-800 p-8 flex-col">
+      <div class="w-full p-4 lg:w-1/3" data-aos="zoom-in" data-aos-delay="500">
+        <div class="flex flex-col h-full p-8 bg-gray-100 rounded-lg dark:bg-gray-800">
           <div class="flex items-center">
             <div
-              class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-tap-blue-500 text-white flex-shrink-0">
+              class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 mr-3 text-white rounded-full bg-tap-blue-500">
               <?php if ($page->is_virtual()->toBool()): ?>
                 <?php snippet('icons/live-fill', ['class' => 'size-5']); ?>
               <?php else: ?>
                 <?php snippet('icons/map-pin-2-fill', ['class' => 'size-5']); ?>
               <?php endif; ?>
             </div>
-            <p class="text-gray-900 dark:text-gray-200 text-lg font-medium"><?= $page->location_name() ?></p>
+            <p class="text-lg font-medium text-gray-900 dark:text-gray-200"><?= $page->location_name() ?></p>
           </div>
         </div>
       </div>
@@ -84,7 +84,7 @@
 </section>
 
 <section class="text-gray-700">
-  <div class="container px-20 sm: px-12 md:px-5 pt-12 pb-24 mx-auto">
+  <div class="container px-12 px-20 pt-12 pb-24 mx-auto sm: md:px-5">
     <div class="flex flex-wrap justify-center -m-4">
       <?php foreach ($page->attendants()->toPages() as $person): ?>
         <?php snippet('person', ['person' => $person]); ?>
@@ -93,15 +93,15 @@
   </div>
 </section>
 
-<nav class="bg-gray-300 dark:bg-gray-800 py-4 px-3 md:px-8">
-  <div class="grid grid-cols-2 divide-x divide-gray-400 dark:divide-gray-700 items-center">
+<nav class="px-3 py-4 bg-gray-300 dark:bg-gray-800 md:px-8">
+  <div class="grid items-center grid-cols-2 divide-x divide-gray-400 dark:divide-gray-700">
     <?php if ($prev = $page->prev()): ?>
       <div class="p-2">
-        <a href="<?= $prev->url() ?>"
-           class="text-gray-600 dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-100 flex flex-row items-center">
+        <a href="<?= $prev->url() ?>" rel="prev"
+           class="flex flex-row items-center text-gray-600 dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-100">
           <?php snippet('icons/arrow-left-s-line', ['class' => 'size-16 float-right mr-4']); ?>
           <div>
-            <p class="text-gray-500 dark:text-gray-400 text-sm uppercase font-medium"><?= $prev->textline() ?></p>
+            <p class="text-sm font-medium text-gray-500 uppercase dark:text-gray-400"><?= $prev->textline() ?></p>
             <p><?= $prev->title() ?></p>
           </div>
         </a>
@@ -111,11 +111,11 @@
     <?php endif; ?>
     <?php if ($next = $page->next()): ?>
       <div class="p-2 text-right">
-        <a href="<?= $next->url() ?>"
-           class="text-gray-600 dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-100 flex flex-row-reverse items-center">
+        <a href="<?= $next->url() ?>" rel="next"
+           class="flex flex-row-reverse items-center text-gray-600 dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-100">
           <?php snippet('icons/arrow-right-s-line', ['class' => 'size-16 float-right ml-4']); ?>
           <div>
-            <p class="text-gray-500 dark:text-gray-400 text-sm uppercase font-medium"><?= $next->textline() ?></p>
+            <p class="text-sm font-medium text-gray-500 uppercase dark:text-gray-400"><?= $next->textline() ?></p>
             <p><?= $next->title() ?></p>
           </div>
         </a>
