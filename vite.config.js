8d0a48c043495da5e04a895ed5de08bda682bf4c
Jens Küsters <jens@kuesters.net>
2025-01-30 13:24:29 +0100

    :recycle: switch to Tailwind Vite plugin, get rid of PostCSS and clean up config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [
+      tailwindcss(),
       laravel({
         hotFile: 'storage/vite.hot',
         buildDirectory: 'build',
-        input: [
-          'resources/css/site.css',
-          'resources/js/site.js',
-          'resources/js/fonts.js',
-        ],
-        refresh: ['site/templates/**', 'site/snippets/**'],
+        input: ['resources/css/site.css', 'resources/js/site.js', 'resources/js/fonts.js'],
+        refresh: true,
         detectTls: 'talk-am-pegel.test',
       }),
     ],
